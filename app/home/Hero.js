633b321015759ca55dad49e0524e921b79d91aaf
TusharU2004 <umretiya123@gmail.com>
2025-11-13 16:31:39 +0530

    improve site performance

--- app/home/Hero.js
+++ app/home/Hero.js
@@ -1,27 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./Hero.module.css"; // Import the CSS module
 
 const Hero = () => {
   return (
-    <section className="hero-section">
+    <section className={styles.heroSection}>
       <Image
         src="/global/hero-image.png"
         alt="Pure and Healthy Groundnut Oil"
         fill={true}
         priority={true}
-        className="hero-background-image"
+        className={styles.heroBackgroundImage}
       />
       {/* This div is the dark overlay for better text readability */}
-      <div className="hero-overlay"></div>
+      <div className={styles.heroOverlay}></div>
 
-      <div className="hero-content">
+      <div className={styles.heroContent}>
         <h1>Pure & Healthy Groundnut Oil</h1>
         <p>
           Our groundnut oil is crafted using a traditional steam heating method,
           ensuring it is 100% pure, chemical-free, and packed with natural
           nutrition for a healthier life.
         </p>
-        <Link href="/product" className="btn">
+        <Link href="/product" className={styles.btn}>
           ➔ ORDER NOW
         </Link>
       </div>
