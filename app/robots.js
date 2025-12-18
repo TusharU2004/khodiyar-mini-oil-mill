d4507aa090f49fb1695987290583fce2a57df654
TusharU2004 <umretiya123@gmail.com>
2025-12-18 17:07:01 +0530

    test 2

---
+++ app/robots.js
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: 'https://khodiyar-oil-mill.onrender.com/sitemap.xml',
+  };
+}
