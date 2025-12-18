6b2ccc4a43b3744d913ca79352b260b4cded7aa9
TusharU2004 <umretiya123@gmail.com>
2025-12-18 16:30:08 +0530

    add seo and site map

---
+++ sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://khodiyar-oil-mill.onrender.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://khodiyar-oil-mill.onrender.com/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://khodiyar-oil-mill.onrender.com/product",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://khodiyar-oil-mill.onrender.com/process",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://khodiyar-oil-mill.onrender.com/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
